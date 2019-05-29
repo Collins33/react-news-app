93041044e34a348811f6fa2f975b05217616492f
collins njau <collinsnjau39@gmail.com>
2019-05-29 10:45:27 +0300

    Configure webpack dev server

---
+++ webpack.config.dev.js
@@ -0,0 +1,51 @@
+// configure webpack for development environment
+/**
+ *use commonjs
+ *node does not support es6
+ */
+const webpack = require("webpack");
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+// declare node environment
+process.env.NODE_ENV = "development";
+
+// configure webpack by exporting js object
+module.exports = {
+  mode: "development",
+  target: "web", // working in the browser
+  devTool: "cheap-module-source-map", // source maps allow us to see our original code in the browser for debugging
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/", // public url of output directory when referenced in the browser
+    fileName: "bundle.js" // html will reference this file from memory
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "src/index.html",
+      favicon: "src/favicon.ico"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
