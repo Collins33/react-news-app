fe47aca7a85d978fb7a17dc17a2affc123f09608
collins njau <collinsnjau39@gmail.com>
2019-05-30 14:13:12 +0300

    Add App file and routes for pages Add header navbar Add page not found component Add courses page

---
+++ src/components/common/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  const activeStyle = { color: "#F15B2A" };
+
+  return (
+    <nav>
+      <NavLink to="/" activeStyle={activeStyle} exact>
+        HOME
+      </NavLink>
+      {" | "}
+      <NavLink to="/about" activeStyle={activeStyle}>
+        ABOUT US
+      </NavLink>
+      {" | "}
+      <NavLink to="/courses" activeStyle={activeStyle}>
+        COURSES
+      </NavLink>
+    </nav>
+  );
+};
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
 
-function Hi() {
-  return <p>Hi.</p>;
-}
+import "./index.css";
 
-render(<Hi />, document.getElementById("app"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
