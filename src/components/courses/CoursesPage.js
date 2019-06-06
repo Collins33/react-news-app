8c53fbb7784efa5a153b4cc7a02aba450f2fb7b5
collins njau <collinsnjau39@gmail.com>
2019-06-06 19:11:06 +0300

    Add form to add courses

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,10 +1,34 @@
 import React, { Component } from "react";
 
 class CoursesPage extends Component {
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
   render() {
     return (
       <div>
-        <h2>Courses</h2>
+        <form onSubmit={this.handleSubmit}>
+          <h2>Courses</h2>
+          <h3>Add course</h3>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.course.title}
+          />
+          <input type="submit" value="save" />
+        </form>
       </div>
     );
   }
