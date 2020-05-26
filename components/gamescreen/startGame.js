4da6cffb4a946e08a96638c30ba096e3db273d78
collins njau <collinsnjau39@gmail.com>
2020-05-26 14:28:46 +0300

    Add styling for the input container

--- components/gamescreen/startGame.js
+++ components/gamescreen/startGame.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, StyleSheet, Text, TextInput, Button} from 'react-native';
+import { black } from 'colorette';
 
 const StartGame = ({gameScreenTitle})=>
 {
@@ -36,7 +37,19 @@ const styles = StyleSheet.create({
   inputContainer:{
     width: 300,
     maxWidth: '80%',
-    alignItems: 'center'
+    alignItems: 'center',
+    // exclusive to android
+    elevation: 8,
+    padding: 20,
+    borderRadius: 10,
+
+    // exclusive styling for IOS
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height:2},
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: 'white'
+
   },
   buttonContainer:{
     flexDirection: 'row',
