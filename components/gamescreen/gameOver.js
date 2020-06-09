1adb88abf9c6c93b8b26c991660ca9089dd73e91
collins njau <collinsnjau39@gmail.com>
2020-06-09 13:30:13 +0300

    Style the gameover image

--- components/gamescreen/gameOver.js
+++ components/gamescreen/gameOver.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { View, StyleSheet, Text, Button, Image} from 'react-native';
 import Card from '../card';
+import { black } from 'ansi-colors';
 
 const GameOver = (props)=>
 {
-  const {screen, buttonContainer, inputContainer, image} = styles;
+  const {screen, buttonContainer, inputContainer, image, imageContainer} = styles;
   return(
   <View style={screen}>
       <Card style={inputContainer}>
         <Text>
           The game is over
         </Text>
+        <View style={imageContainer}>
         <Image source={require('../../assets/original.png')} style={image}/>
+        </View>
         <Text>
           Rounds taken:{props.roundsTaken}
         </Text>
@@ -45,9 +48,18 @@ const styles = StyleSheet.create({
     maxWidth: '80%',
     alignItems: 'center',
   },
+  imageContainer:{
+    width:300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth:3,
+    borderColor: 'black',
+    overflow: 'hidden',
+    marginVertical: 30
+  },
   image:{
-    width:'80%',
-    height: 300
+    width:'100%',
+    height: '100%'
   }
 })
 
